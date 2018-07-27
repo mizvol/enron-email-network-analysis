9454f03622b6b8ed6a7352c1d53cb17f23682df1
Unknown <mizvladimir@gmail.com>
2018-07-27 14:57:13 +0200

    Read files and build a graph with activations

--- EnronNet/src/main/scala/BuildGraph.scala
+++ EnronNet/src/main/scala/BuildGraph.scala
@@ -1,6 +1,8 @@
 import org.apache.spark.sql.SparkSession
 import ch.epfl.lts2.Utils._
 import org.slf4j.{Logger, LoggerFactory}
+import org.apache.spark.graphx.{Edge, Graph, VertexId}
+import org.apache.spark.rdd.RDD
 
 object BuildGraph {
   def main(args: Array[String]): Unit = {
@@ -14,32 +16,50 @@ object BuildGraph {
       .config("spark.executor.memory", "50g")
       .getOrCreate()
 
+    import spark.implicits._
+
     val log: Logger = LoggerFactory.getLogger(this.getClass)
     log.info("Start")
 
     val PATH_RESOURCES: String = "/mnt/data/git/enron-email-network-analysis/EnronNet/src/main/resources/"
+    val DAYS_TOTAL: Int = 2221
 
     val edgesDF = spark.sqlContext.read
       .format("com.databricks.spark.csv")
       .options(Map("header"-> "true", "inferSchema"-> "true"))
-      .load(PATH_RESOURCES + "weighted_links_activated.csv")
-
-    edgesDF.show()
+      .load(PATH_RESOURCES + "weighted_links_activated_id.csv")
 
     log.info(edgesDF.count() + " links in the network")
 
     val activationsDF = spark.sqlContext.read
       .format("com.databricks.spark.csv")
       .options(Map("header"-> "false", "inferSchema"-> "true"))
-      .load(PATH_RESOURCES + "activations-enron.csv")
+      .load(PATH_RESOURCES + "activations-enron_id.csv")
       .withColumnRenamed("_c0", "Email")
       .withColumnRenamed("_c1", "Activations")
 
-    activationsDF.show()
-
     log.info(activationsDF.count() + " emails in the network")
 
+    val edgesRDD: RDD[Edge[Double]] = edgesDF.as[(String, String, String)].rdd.coalesce(12).map(e => Edge(e._1.toLong, e._2.toLong, 0.0))
 
+    log.info(edgesRDD.count() + " edges in RDD")
+
+    def parseDict(stringMap: String): Map[Int, Double] = {
+      stringMap.substring(0, stringMap.length)
+        .split(", ")
+        .map(_.split(": "))
+        .map { case Array(k, v) => (k.substring(0, k.length).replace("{", "").toInt, v.substring(0, v.length).replace("}", "").toDouble)}
+        .toMap
+    }
+
+    val verticesRDD: RDD[(VertexId, (Map[Int, Double]))] = activationsDF.as[(String, String)].rdd.map(v => (v._1.toLong, parseDict(v._2)))
+
+    log.info(verticesRDD.count() + " vertices in RDD")
+
+    val graph = Graph(verticesRDD, edgesRDD)
+
+    log.info(graph.edges.count() + " edges and " + graph.vertices.count() + " vertices in the graph")
+
+    
   }
-
 }
