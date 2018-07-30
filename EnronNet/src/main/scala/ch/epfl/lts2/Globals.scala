14c3f3130e1a069a040a067384b26eacc5a02ba0
Unknown <mizvladimir@gmail.com>
2018-07-30 17:02:24 +0200

    December 1999 timestamps

--- EnronNet/src/main/scala/ch/epfl/lts2/Globals.scala
+++ EnronNet/src/main/scala/ch/epfl/lts2/Globals.scala
@@ -8,5 +8,6 @@ package object Globals {
   val START_TIME = 0 // start date: 06 Jan 1998
   val END_TIME = 2221 // end date: 04 Feb 2004
 
-
+  val DEC_99_START = 359 + 365 - 31 // 359 days in 1998 + 365 days in 1999 - 31 days of December
+  val DEC_99_END = 359 + 365 // 359 days in 1998 + 365 days in 1999
 }
