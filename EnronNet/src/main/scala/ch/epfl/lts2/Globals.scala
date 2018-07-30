d28446a47a8e8dc21b040e465b790f427924c86a
Unknown <mizvladimir@gmail.com>
2018-07-30 16:58:07 +0200

    Global variables

---
+++ EnronNet/src/main/scala/ch/epfl/lts2/Globals.scala
@@ -0,0 +1,12 @@
+package ch.epfl.lts2
+
+/**
+  * Created by volodymyrmiz on 30/07/18.
+  */
+package object Globals {
+  // timestamps are in days, e.g. 2221 days in total
+  val START_TIME = 0 // start date: 06 Jan 1998
+  val END_TIME = 2221 // end date: 04 Feb 2004
+
+
+}
